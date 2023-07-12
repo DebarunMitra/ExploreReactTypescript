f9f75f59efa780fa9c165adb612b91072b9dc2cb
DebarunMitra <debarun96mitra@gmail.com>
2023-07-12 11:39:44 +0530

    initial routes and nav created

---
+++ shopping-cart/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import {Container,Navbar as NavBS} from 'react-bootstrap' 
+
+export function Navbar() {
+    return (
+        <NavBS className = "bg-white shadow-sm mb-3">
+           <Container>
+                Nav
+           </Container>
+        </NavBS>
+    )
+}
