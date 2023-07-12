ee7ba988a5ec43e3bfff4a412e4491c97dc740b4
DebarunMitra <debarun96mitra@gmail.com>
2023-07-12 13:47:46 +0530

    currency formatter added

--- shopping-cart/src/components/StoreItem.tsx
+++ shopping-cart/src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { formatCurrency } from '../utilities/formatCurrency'
 
 type StoreItemProps = {
   id: number,
@@ -25,7 +26,7 @@ function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
           className="d-flex justify-content-between align-items-baseline mb-4"
         >
           <span className="fs-2">{name}</span>
-          <span className="ms-2 text-muted">{price}</span>
+          <span className="ms-2 text-muted">{ formatCurrency(price)}</span>
         </Card.Title>
       </Card.Body>
     
