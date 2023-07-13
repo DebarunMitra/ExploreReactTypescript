d076e792bbc0d0da46eefd1d954d000567fbd0b3
DebarunMitra <debarun96mitra@gmail.com>
2023-07-13 18:23:54 +0530

    nav bar cart handlers created

--- shopping-cart/src/context/ShoppingCartContext.tsx
+++ shopping-cart/src/context/ShoppingCartContext.tsx
@@ -11,10 +11,14 @@ type CartItem = {
 
 
 type ShoppingCartContext = {
+    openCart: () => void
+    closeCart: () => void
      getItemQuantity: (id: number) => number
      increaseCartQuantity: (id: number) => void
      decreaseCartQuantity: (id: number) => void
      removeFormCart: (id: number) => void
+    cartQuantiry: number
+    cartItems: CartItem[]
 }
 
 
@@ -26,6 +30,15 @@ export function useShoppingCart(){
 
 export function ShoppingCartProvider({children}: ShoppingCartProviderProps){
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openCart = () => {
+        setIsOpen(true)
+    }
+
+    const closeCart = () => {
+        setIsOpen(false)
+    }
 
     function getItemQuantity(id: number) {
         return cartItems.find(item => item.id === id)?.quantity || 0;
@@ -69,6 +82,8 @@ export function ShoppingCartProvider({children}: ShoppingCartProviderProps){
         })
     }
 
+    const cartQuantity = cartItems.reduce((quantity, item)=> item.quantity+ quantity, 0);
+
 
 
     return (
@@ -76,7 +91,11 @@ export function ShoppingCartProvider({children}: ShoppingCartProviderProps){
             getItemQuantity, 
             increaseCartQuantity, 
             decreaseCartQuantity,
-            removeFromCart
+            removeFromCart,
+            cartItems,
+            cartQuantity,
+            openCart,
+            closeCart
         }}>
             {children}
         </ShoppingCartContext.Provider>
